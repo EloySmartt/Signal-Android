70d464a764597e50084f43475e17d82c7e27d926
EloySmartt <eloy@smarttmessenger.com>
2026-09-01 10:00:40 +0200

    New internal test version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ apply(from = "static-ips.gradle.kts")
 
 val canonicalVersionCode = 1568
 val canonicalVersionName = "7.51.3"
-val currentHotfixVersion = 6
+val currentHotfixVersion = 8
 val maxHotfixVersions = 100
 
 val keystores: Map<String, Properties?> = mapOf("debug" to loadKeystoreProperties("keystore.debug.properties"))
